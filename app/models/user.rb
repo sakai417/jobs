4a71d63a6b67ecc15d00e3be00808e47d2d45c04
sakai417 <sakai.417.tblbj@gmail.com>
2016-03-22 21:46:01 +0900

    avatarのアップロードをaws-sdkからfogに変更した

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,17 @@ class User < ActiveRecord::Base
                       medium: "300x300#",
                       thumb: "100x100#",
                     },
-                    :storage => :s3,
-                    :s3_permissions => :public,
-                    :s3_credentials => "#{Rails.root}/config/s3.yml",
-                    :path => ":attachment/:id/:style.:extension"
+                    storage: :fog,
+                    fog_credentials: {
+                      aws_access_key_id: Settings.jobs[:aws_access_key_id],
+                      aws_secret_access_key: Settings.jobs[:aws_secret_access_key],
+                      region: Settings.jobs[:region],
+                      provider: 'AWS',
+                      scheme: 'https'
+                    },
+                    fog_directory: 'jobs-image',
+                    path: 'uploads/user/avatar/:id/:hash_:style.:extension',
+                    hash_secret: ENV['SECRET_KEY_BASE']
   validates_attachment_content_type :avatar,        content_type: ["image/jpg","image/jpeg","image/png"]
   # do_not_validate_attachment_file_type :image
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Settings.jobs[:aws_access_key_id],
     aws_secret_access_key: Settings.jobs[:aws_secret_access_key],
-    region: 'us-east-1'
+    region: Settings.jobs[:region]
   }
 
   case Rails.env
